0d3936f66dea3150c63b34cd2dad920c82430b7f
Vimukthi Saranga <vimukthisaranga1@gmail.com>
2024-01-22 18:58:59 +0300

    Update TestData.java

--- src/test/java/com/qa/tests/insuranceForm/TestData.java
+++ src/test/java/com/qa/tests/insuranceForm/TestData.java
@@ -4,8 +4,8 @@ import com.qa.base.BaseTest;
 
 public class TestData extends BaseTest {
     String Url = "https://www.appsheet.com/Account/Login?returnUrl=https%3A%2F%2Fwww.appsheet.com%2Fstart%2F422a90c2-80c8-41e3-8561-c7692d9762cf&provider=google&fullScope=False&appName=Life%20Insurance%20-%20Proposal%20Form";
-    String userName = "developmentaarav@gmail.com";
-    String password = "Agspl@2021";
+    String userName = "";
+    String password = "";
 
     public String getUrl() {
         return Url;
